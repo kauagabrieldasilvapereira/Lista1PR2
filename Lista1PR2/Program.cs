6960fe0f13457c5490cb5d6b8dcd052026d63cf6
Kauã <Kau@DESKTOP-7MPTE86>
2023-03-01 16:18:55 -0300

    Adicionar arquivos de projeto.

---
+++ Lista1PR2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1PR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //---------------------------------------------------------------------------------------------------------
+
+            //        Programa1
+
+            //        int metro, mm, cm;
+
+            //        Console.WriteLine("Quantos metros : ");
+            //        metro = int.Parse(Console.ReadLine()); 
+
+            //        mm = metro * 1000;
+            //        cm = metro * 100;
+
+            //        Console.WriteLine("São " + cm + " centimetros" );
+            //        Console.WriteLine("São " + mm + " milimetros"  );
+
+
+            //        Console.ReadKey();  
+
+
+
+            //--------------------------------------------------------------------------------------------------------------------
+
+            //Programa2 a
+
+            //float f, c;
+
+            //Console.WriteLine("Quantos graus fahrenheit são ? ");
+            //f = int.Parse(Console.ReadLine());
+            //c = (f - 32.0f) * (5.0f / 9.0f);
+
+            //Console.WriteLine("São " + c + "° Celsius");
+            //Console.ReadKey();
+
+            //-------------------------------------------------------------------------------------------------------------------
+
+            //Programa2 b
+
+            //int f, c;
+
+            //Console.WriteLine("Quantos graus fahrenheit são ? ");
+            //f = int.Parse(Console.ReadLine());
+            //c = (f - 32) * (5 / 9);
+
+            //Console.WriteLine("São " + c + "° Celsius");
+            //Console.ReadKey();
+
+            //O valor sempre da zero por conta de ser somente o float quem trabalha com virgulas
+
+            //-------------------------------------------------------------------------------------------------------------------
+
+            //Programa3 
+
+            //double Imc, peso, alt;
+
+            //Console.WriteLine("Qual seu Peso ? ");
+            //peso = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Qual sua altura ? ");
+            //alt = double.Parse(Console.ReadLine());
+
+            //Imc = peso / (alt * alt);
+
+            //Console.WriteLine("Seu índice de massa corporal é " + Imc);
+            //Console.ReadKey();
+
+            //------------------------------------------------------------------------------------------------------------------
+
+            //Programa4
+
+            //float num1, num2, num3, peso1, peso2, peso3, result;
+
+            //Console.WriteLine("Digite um número: ");
+            //num1 = float.Parse(Console.ReadLine());  
+
+            //Console.WriteLine("Digite um número: ");
+            //num2 = float.Parse(Console.ReadLine());  
+
+            //Console.WriteLine("Digite um número: ");
+            //num3 = float.Parse(Console.ReadLine());  
+
+            //Console.WriteLine("Digite um peso: ");
+            //peso1 = float.Parse(Console.ReadLine());  
+
+            //Console.WriteLine("Digite um peso: ");
+            //peso2 = float.Parse(Console.ReadLine());  
+
+            //Console.WriteLine("Digite um peso: ");
+            //peso3 = float.Parse(Console.ReadLine());  
+
+            //result = (num1 + num2 + num3 + peso1 + peso2 + peso3);
+
+
+            //Console.WriteLine("O valor da média é: " + result / 6.0f);
+
+
+            //Console.ReadKey();  
+
+            //---------------------------------------------------------------------------------------------------------------
+
+            //Programa5
+        }
+    }
+}
